fe700a929b9a0af405de688910cdd1d5cc9e11c4
GhostCDXI <96083271+GhostCDXI@users.noreply.github.com>
2026-07-06 00:16:36 +0700

    Add files via upload

---
+++ idm/ias.cmd
@@ -0,0 +1,500 @@
+@set iasver=1.2
+@setlocal DisableDelayedExpansion
+@echo off
+
+
+
+::  To reset the activation and trial, run the script with "/res" parameter or change 0 to 1 in below line
+set _reset=1
+
+::  If value is changed in above lines or parameter is used then script will run in unattended mode
+
+::========================================================================================================================================
+
+::  Set Path variable, it helps if it is misconfigured in the system
+
+set "PATH=%SystemRoot%\System32;%SystemRoot%\System32\wbem;%SystemRoot%\System32\WindowsPowerShell\v1.0\"
+if exist "%SystemRoot%\Sysnative\reg.exe" (
+set "PATH=%SystemRoot%\Sysnative;%SystemRoot%\Sysnative\wbem;%SystemRoot%\Sysnative\WindowsPowerShell\v1.0\;%PATH%"
+)
+
+:: Re-launch the script with x64 process if it was initiated by x86 process on x64 bit Windows
+:: or with ARM64 process if it was initiated by x86/ARM32 process on ARM64 Windows
+
+set "_cmdf=%~f0"
+for %%# in (%*) do (
+if /i "%%#"=="r1" set r1=1
+if /i "%%#"=="r2" set r2=1
+)
+
+if exist %SystemRoot%\Sysnative\cmd.exe if not defined r1 (
+setlocal EnableDelayedExpansion
+start %SystemRoot%\Sysnative\cmd.exe /c ""!_cmdf!" %* r1"
+exit /b
+)
+
+:: Re-launch the script with ARM32 process if it was initiated by x64 process on ARM64 Windows
+
+if exist %SystemRoot%\SysArm32\cmd.exe if %PROCESSOR_ARCHITECTURE%==AMD64 if not defined r2 (
+setlocal EnableDelayedExpansion
+start %SystemRoot%\SysArm32\cmd.exe /c ""!_cmdf!" %* r2"
+exit /b
+)
+
+::========================================================================================================================================
+
+set "blank="
+set "mas=ht%blank%tps%blank%://mass%blank%grave.dev/"
+
+::  Check if Null service is working, it's important for the batch script
+
+sc query Null | find /i "RUNNING"
+if %errorlevel% NEQ 0 (
+echo:
+echo Null service is not running, script may crash...
+echo:
+echo:
+echo Help - %mas%idm-activation-script.html#Troubleshoot
+echo:
+echo:
+ping 127.0.0.1 -n 10
+)
+cls
+
+::  Check LF line ending
+
+pushd "%~dp0"
+>nul findstr /v "$" "%~nx0" && (
+echo:
+echo Error: Script either has LF line ending issue or an empty line at the end of the script is missing.
+echo:
+ping 127.0.0.1 -n 6 >nul
+popd
+exit /b
+)
+popd
+
+::========================================================================================================================================
+
+cls
+color 07
+title  IDM Activation Script %iasver%
+
+set _args=
+set _elev=
+set _unattended=0
+
+set _args=%*
+if defined _args set _args=%_args:"=%
+if defined _args (
+for %%A in (%_args%) do (
+if /i "%%A"=="-el"  set _elev=1
+if /i "%%A"=="/res" set _reset=1
+)
+)
+
+for %%A in (%_activate% %_freeze% %_reset%) do (if "%%A"=="1" set _unattended=1)
+
+::========================================================================================================================================
+
+set "nul1=1>nul"
+set "nul2=2>nul"
+set "nul6=2^>nul"
+set "nul=>nul 2>&1"
+
+set psc=powershell.exe
+set winbuild=1
+for /f "tokens=6 delims=[]. " %%G in ('ver') do set winbuild=%%G
+
+set _NCS=1
+if %winbuild% LSS 10586 set _NCS=0
+if %winbuild% GEQ 10586 reg query "HKCU\Console" /v ForceV2 %nul2% | find /i "0x0" %nul1% && (set _NCS=0)
+
+if %_NCS% EQU 1 (
+for /F %%a in ('echo prompt $E ^| cmd') do set "esc=%%a"
+set     "Red="41;97m""
+set    "Gray="100;97m""
+set   "Green="42;97m""
+set    "Blue="44;97m""
+set  "_White="40;37m""
+set  "_Green="40;92m""
+set "_Yellow="40;93m""
+) else (
+set     "Red="Red" "white""
+set    "Gray="Darkgray" "white""
+set   "Green="DarkGreen" "white""
+set    "Blue="Blue" "white""
+set  "_White="Black" "Gray""
+set  "_Green="Black" "Green""
+set "_Yellow="Black" "Yellow""
+)
+
+set "nceline=echo: &echo ==== ERROR ==== &echo:"
+set "eline=echo: &call :_color %Red% "==== ERROR ====" &echo:"
+set "line=___________________________________________________________________________________________________"
+set "_buf={$W=$Host.UI.RawUI.WindowSize;$B=$Host.UI.RawUI.BufferSize;$W.Height=34;$B.Height=300;$Host.UI.RawUI.WindowSize=$W;$Host.UI.RawUI.BufferSize=$B;}"
+
+::========================================================================================================================================
+
+if %winbuild% LSS 7600 (
+%nceline%
+echo Unsupported OS version Detected [%winbuild%].
+echo Project is supported only for Windows 7/8/8.1/10/11 and their Server equivalent.
+goto done2
+)
+
+for %%# in (powershell.exe) do @if "%%~$PATH:#"=="" (
+%nceline%
+echo Unable to find powershell.exe in the system.
+goto done2
+)
+
+::========================================================================================================================================
+
+::  Fix for the special characters limitation in path name
+
+set "_work=%~dp0"
+if "%_work:~-1%"=="\" set "_work=%_work:~0,-1%"
+
+set "_batf=%~f0"
+set "_batp=%_batf:'=''%"
+
+set _PSarg="""%~f0""" -el %_args%
+set _PSarg=%_PSarg:'=''%
+
+set "_appdata=%appdata%"
+set "_ttemp=%userprofile%\AppData\Local\Temp"
+
+setlocal EnableDelayedExpansion
+
+::========================================================================================================================================
+
+echo "!_batf!" | find /i "!_ttemp!" %nul1% && (
+if /i not "!_work!"=="!_ttemp!" (
+%eline%
+echo Script is launched from the temp folder,
+echo Most likely you are running the script directly from the archive file.
+echo:
+echo Extract the archive file and launch the script from the extracted folder.
+goto done2
+)
+)
+
+::========================================================================================================================================
+
+::  Check PowerShell
+
+REM :PowerShellTest: $ExecutionContext.SessionState.LanguageMode :PowerShellTest:
+
+%psc% "$f=[io.file]::ReadAllText('!_batp!') -split ':PowerShellTest:\s*';iex ($f[1])" | find /i "FullLanguage" %nul1% || (
+%eline%
+%psc% $ExecutionContext.SessionState.LanguageMode
+echo:
+echo PowerShell is not working. Aborting...
+echo If you have applied restrictions on Powershell then undo those changes.
+echo:
+echo Check this page for help. %mas%idm-activation-script.html#Troubleshoot
+goto done2
+)
+
+::========================================================================================================================================
+
+::  Elevate script as admin and pass arguments and preventing loop
+
+%nul1% fltmc || (
+if not defined _elev %psc% "start cmd.exe -arg '/c \"!_PSarg!\"' -verb runas" && exit /b
+%eline%
+echo This script requires admin privileges.
+echo To do so, right click on this script and select 'Run as administrator'.
+goto done2
+)
+
+::========================================================================================================================================
+
+::  Disable QuickEdit and launch from conhost.exe to avoid Terminal app
+
+set quedit=
+set terminal=
+
+if %_unattended%==1 (
+set quedit=1
+set terminal=1
+)
+
+for %%# in (%_args%) do (if /i "%%#"=="-qedit" set quedit=1)
+
+if %winbuild% LSS 10586 (
+reg query HKCU\Console /v QuickEdit %nul2% | find /i "0x0" %nul1% && set quedit=1
+)
+
+if %winbuild% GEQ 17763 (
+set "launchcmd=start conhost.exe %psc%"
+) else (
+set "launchcmd=%psc%"
+)
+
+set "d1=$t=[AppDomain]::CurrentDomain.DefineDynamicAssembly(4, 1).DefineDynamicModule(2, $False).DefineType(0);"
+set "d2=$t.DefinePInvokeMethod('GetStdHandle', 'kernel32.dll', 22, 1, [IntPtr], @([Int32]), 1, 3).SetImplementationFlags(128);"
+set "d3=$t.DefinePInvokeMethod('SetConsoleMode', 'kernel32.dll', 22, 1, [Boolean], @([IntPtr], [Int32]), 1, 3).SetImplementationFlags(128);"
+set "d4=$k=$t.CreateType(); $b=$k::SetConsoleMode($k::GetStdHandle(-10), 0x0080);"
+
+if defined quedit goto :skipQE
+%launchcmd% "%d1% %d2% %d3% %d4% & cmd.exe '/c' '!_PSarg! -qedit'" &exit /b
+:skipQE
+
+::========================================================================================================================================
+
+::  Check for updates
+
+set -=
+set old=
+
+for /f "delims=[] tokens=2" %%# in ('ping -4 -n 1 iasupdatecheck.mass%-%grave.dev') do (
+if not [%%#]==[] (echo "%%#" | find "127.69" %nul1% && (echo "%%#" | find "127.69.%iasver%" %nul1% || set old=1))
+)
+
+if defined old (
+echo ________________________________________________
+%eline%
+echo You are running outdated version IAS %iasver%
+echo ________________________________________________
+echo:
+if not %_unattended%==1 (
+echo [1] Get Latest IAS
+echo [0] Continue Anyway
+echo:
+call :_color %_Green% "Enter a menu option in the Keyboard [1,0] :"
+choice /C:10 /N
+if !errorlevel!==2 rem
+if !errorlevel!==1 (start https://github.com/WindowsAddict/IDM-Activation-Script & start %mas%/idm-activation-script & exit /b)
+)
+)
+
+::========================================================================================================================================
+
+cls
+title  IDM Activation Script %iasver%
+
+echo:
+echo Initializing...
+
+::  Check WMI
+
+%psc% "Get-WmiObject -Class Win32_ComputerSystem | Select-Object -Property CreationClassName" %nul2% | find /i "computersystem" %nul1% || (
+%eline%
+%psc% "Get-WmiObject -Class Win32_ComputerSystem | Select-Object -Property CreationClassName"
+echo:
+echo WMI is not working. Aborting...
+echo:
+echo Check this page for help. %mas%idm-activation-script.html#Troubleshoot
+goto done2
+)
+
+::  Check user account SID
+
+set _sid=
+for /f "delims=" %%a in ('%psc% "([System.Security.Principal.NTAccount](Get-WmiObject -Class Win32_ComputerSystem).UserName).Translate([System.Security.Principal.SecurityIdentifier]).Value" %nul6%') do (set _sid=%%a)
+ 
+reg query HKU\%_sid%\Software %nul% || (
+for /f "delims=" %%a in ('%psc% "$explorerProc = Get-Process -Name explorer | Where-Object {$_.SessionId -eq (Get-Process -Id $pid).SessionId} | Select-Object -First 1; $sid = (gwmi -Query ('Select * From Win32_Process Where ProcessID=' + $explorerProc.Id)).GetOwnerSid().Sid; $sid" %nul6%') do (set _sid=%%a)
+)
+
+reg query HKU\%_sid%\Software %nul% || (
+%eline%
+echo:
+echo [%_sid%]
+echo User Account SID not found. Aborting...
+echo:
+echo Check this page for help. %mas%idm-activation-script.html#Troubleshoot
+goto done2
+)
+
+::========================================================================================================================================
+
+::  Check if the current user SID is syncing with the HKCU entries
+
+%nul% reg delete HKCU\IAS_TEST /f
+%nul% reg delete HKU\%_sid%\IAS_TEST /f
+
+set HKCUsync=$null
+%nul% reg add HKCU\IAS_TEST
+%nul% reg query HKU\%_sid%\IAS_TEST && (
+set HKCUsync=1
+)
+
+%nul% reg delete HKCU\IAS_TEST /f
+%nul% reg delete HKU\%_sid%\IAS_TEST /f
+
+::  Below code also works for ARM64 Windows 10 (including x64 bit emulation)
+
+for /f "skip=2 tokens=2*" %%a in ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v PROCESSOR_ARCHITECTURE') do set arch=%%b
+if /i not "%arch%"=="x86" set arch=x64
+
+if "%arch%"=="x86" (
+set "CLSID=HKCU\Software\Classes\CLSID"
+set "CLSID2=HKU\%_sid%\Software\Classes\CLSID"
+set "HKLM=HKLM\Software\Internet Download Manager"
+) else (
+set "CLSID=HKCU\Software\Classes\Wow6432Node\CLSID"
+set "CLSID2=HKU\%_sid%\Software\Classes\Wow6432Node\CLSID"
+set "HKLM=HKLM\SOFTWARE\Wow6432Node\Internet Download Manager"
+)
+
+for /f "tokens=2*" %%a in ('reg query "HKU\%_sid%\Software\DownloadManager" /v ExePath %nul6%') do call set "IDMan=%%b"
+
+if not exist "%IDMan%" (
+if %arch%==x64 set "IDMan=%ProgramFiles(x86)%\Internet Download Manager\IDMan.exe"
+if %arch%==x86 set "IDMan=%ProgramFiles%\Internet Download Manager\IDMan.exe"
+)
+
+if not exist %SystemRoot%\Temp md %SystemRoot%\Temp
+set "idmcheck=tasklist /fi "imagename eq idman.exe" | findstr /i "idman.exe" %nul1%"
+
+::  Check CLSID registry access
+
+%nul% reg add %CLSID2%\IAS_TEST
+%nul% reg query %CLSID2%\IAS_TEST || (
+%eline%
+echo Failed to write in %CLSID2%
+echo:
+echo Check this page for help. %mas%idm-activation-script.html#Troubleshoot
+goto done2
+)
+
+%nul% reg delete %CLSID2%\IAS_TEST /f
+
+::========================================================================================================================================
+
+if %_reset%==1 goto :_reset
+
+:MainMenu
+
+cls
+title  IDM Activation Script %iasver%
+if not defined terminal mode 75, 28
+
+echo:
+echo:
+echo:
+echo:
+echo:
+echo:                This script is NOT working with latest IDM.     
+echo:            ___________________________________________________ 
+echo:                                                               
+echo:               [1] Freeze Trial
+echo:               [2] Activate
+echo:               [3] Reset Activation / Trial
+echo:               _____________________________________________   
+echo:                                                               
+echo:               [4] Download IDM
+echo:               [5] Help
+echo:               [0] Exit
+echo:            ___________________________________________________
+echo:         
+call :_color2 %_White% "             " %_Green% "Enter a menu option in the Keyboard [1,2,3,4,5,0]"
+choice /C:123450 /N
+set _erl=%errorlevel%
+
+if %_erl%==6 exit /b
+if %_erl%==5 start https://github.com/WindowsAddict/IDM-Activation-Script & start https://massgrave.dev/idm-activation-script & goto MainMenu
+if %_erl%==4 start https://www.internetdownloadmanager.com/download.html & goto MainMenu
+if %_erl%==3 goto _reset
+if %_erl%==2 (set frz=0&goto :_activate)
+if %_erl%==1 (set frz=1&goto :_activate)
+goto :MainMenu
+
+::========================================================================================================================================
+
+:_reset
+
+cls
+if not %HKCUsync%==1 (
+if not defined terminal mode 153, 35
+) else (
+if not defined terminal mode 113, 35
+)
+if not defined terminal %psc% "&%_buf%" %nul%
+
+echo:
+%idmcheck% && taskkill /f /im idman.exe
+
+set _time=
+for /f %%a in ('%psc% "(Get-Date).ToString('yyyyMMdd-HHmmssfff')"') do set _time=%%a
+
+echo:
+echo Creating backup of CLSID registry keys in %SystemRoot%\Temp
+
+reg export %CLSID% "%SystemRoot%\Temp\_Backup_HKCU_CLSID_%_time%.reg"
+if not %HKCUsync%==1 reg export %CLSID2% "%SystemRoot%\Temp\_Backup_HKU-%_sid%_CLSID_%_time%.reg"
+
+call :delete_queue
+%psc% "$sid = '%_sid%'; $HKCUsync = %HKCUsync%; $lockKey = $null; $deleteKey = 1; $f=[io.file]::ReadAllText('!_batp!') -split ':regscan\:.*';iex ($f[1])"
+
+call :add_key
+
+echo:
+echo %line%
+echo:
+call :_color %Green% "The IDM reset process has been completed."
+
+goto done
+
+:delete_queue
+
+echo:
+echo Deleting IDM registry keys...
+echo:
+
+for %%# in (
+""HKCU\Software\DownloadManager" "/v" "FName""
+""HKCU\Software\DownloadManager" "/v" "LName""
+""HKCU\Software\DownloadManager" "/v" "Email""
+""HKCU\Software\DownloadManager" "/v" "Serial""
+""HKCU\Software\DownloadManager" "/v" "scansk""
+""HKCU\Software\DownloadManager" "/v" "tvfrdt""
+""HKCU\Software\DownloadManager" "/v" "radxcnt""
+""HKCU\Software\DownloadManager" "/v" "LstCheck""
+""HKCU\Software\DownloadManager" "/v" "ptrk_scdt""
+""HKCU\Software\DownloadManager" "/v" "LastCheckQU""
+"%HKLM%"
+) do for /f "tokens=* delims=" %%A in ("%%~#") do (
+set "reg="%%~A"" &reg query !reg! %nul% && call :del
+)
+
+if not %HKCUsync%==1 for %%# in (
+""HKU\%_sid%\Software\DownloadManager" "/v" "FName""
+""HKU\%_sid%\Software\DownloadManager" "/v" "LName""
+""HKU\%_sid%\Software\DownloadManager" "/v" "Email""
+""HKU\%_sid%\Software\DownloadManager" "/v" "Serial""
+""HKU\%_sid%\Software\DownloadManager" "/v" "scansk""
+""HKU\%_sid%\Software\DownloadManager" "/v" "tvfrdt""
+""HKU\%_sid%\Software\DownloadManager" "/v" "radxcnt""
+""HKU\%_sid%\Software\DownloadManager" "/v" "LstCheck""
+""HKU\%_sid%\Software\DownloadManager" "/v" "ptrk_scdt""
+""HKU\%_sid%\Software\DownloadManager" "/v" "LastCheckQU""
+) do for /f "tokens=* delims=" %%A in ("%%~#") do (
+set "reg="%%~A"" &reg query !reg! %nul% && call :del
+)
+
+exit /b
+
+:del
+
+reg delete %reg% /f %nul%
+
+if "%errorlevel%"=="0" (
+set "reg=%reg:"=%"
+echo Deleted - !reg!
+) else (
+set "reg=%reg:"=%"
+call :_color2 %Red% "Failed - !reg!"
+)
+
+exit /b
+
+::========================================================================================================================================
+
+
+
+::========================================================================================================================================
+:: Leave empty line below
